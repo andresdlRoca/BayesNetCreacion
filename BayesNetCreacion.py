20ef7549617efccf6fab9435ba5b69c9acf1dcb1
andresdlRoca <andresdelaroca11@gmail.com>
2023-02-08 14:02:30 -0600

    Initial Commit

---
+++ BayesNetCreacion.py
@@ -0,0 +1,27 @@
+# Base
+
+class BayesNetCreacion():
+    def __init__(self):
+        pass
+
+class BayesNetwork():
+    def __init__(self):
+        pass
+
+class Node():
+    def __init__(self):
+        pass
+
+#Additional services
+
+def descriptiveCheck():
+    pass
+
+def compact():
+    pass
+
+def getFactors():
+    pass
+
+def getEnum():
+    pass
